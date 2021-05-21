0e46726f3b5e55ee7a11821adf17a3123c65fa01
Christelle Couchoux <christelle.couchoux@gmail.com>
2021-05-21 17:35:37 +0200

    add general style (color palette, typography, header, footer, titles) and index page style

---
+++ js/helpers/Camera.js
@@ -0,0 +1,8 @@
+class Camera {
+    constructor (id, name, price, lenses) {
+        this.id = id,
+        this.name = name,
+        this.price = price,
+        this.lenses = lenses
+    }    
+}
